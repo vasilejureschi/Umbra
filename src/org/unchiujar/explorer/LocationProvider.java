1a9dcaa2b771d52e32fb52ba2f72fcd8f3a99e0b
Vasile Jureschi <vasile.jureschi@gmail.com>
2011-08-19 11:15:49 +0300

    Added comment

--- src/org/unchiujar/explorer/LocationProvider.java
+++ src/org/unchiujar/explorer/LocationProvider.java
@@ -6,5 +6,12 @@ interface LocationProvider {
     long insert(AproximateLocation location);
     void deleteAll();
     List<AproximateLocation> selectAll();
+    /**
+     * Returns a list of visited points in the specified area. The coordinate system used
+     * is the latitude longitude decimal system. 
+     * @param upperLeft the upper left coordinates using the latitude/longitude system 
+     * @param bottomRight the bottom right coordinates using the latitude/longitude system
+     * @return a List of visited locations
+     */
     List<AproximateLocation> selectVisited(AproximateLocation upperLeft, AproximateLocation bottomRight);
 }
