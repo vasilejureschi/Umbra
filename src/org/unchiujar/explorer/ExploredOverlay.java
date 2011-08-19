c7220ec58c711292b296a43bc3744cdbd88e5876
Vasile Jureschi <vasile.jureschi@gmail.com>
2011-08-19 10:48:43 +0300

    Simplified code by removing list copying.

--- src/org/unchiujar/explorer/ExploredOverlay.java
+++ src/org/unchiujar/explorer/ExploredOverlay.java
@@ -19,51 +19,66 @@ import android.util.Log;
 import com.google.android.maps.GeoPoint;
 import com.google.android.maps.MapView;
 import com.google.android.maps.Overlay;
+import com.google.android.maps.Projection;
 
 public class ExploredOverlay extends Overlay {
     private static final String TAG = ExploredOverlay.class.getName();
-    private List<GeoPoint> geoPoints = new ArrayList<GeoPoint>();
+    private List<AproximateLocation> locations;
     private Context context;
-    private List<Point> screenPts;
     private boolean pointsUpdated;
+    private Paint rectPaint;
+    private double currentLat;
+    private double currentLong;
+
+    // TODO remove - DEBUG code
+    int i;
+    Paint paint1 = new Paint();
+    // TODO remove - DEBUG code
+    private Paint currentPaint;
+    private Paint circlePaint;
+    private Point tempPoint = new Point();
 
     public ExploredOverlay(Context context) {
         this.context = context;
+
+        rectPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        rectPaint.setColor(Color.DKGRAY);
+        rectPaint.setAlpha(50);
+        rectPaint.setStyle(Style.FILL_AND_STROKE);
+
+        currentPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        currentPaint.setColor(Color.BLUE);
+        currentPaint.setStyle(Style.STROKE);
+        
+        circlePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        // set PorterDuff mode in order to create transparent holes in
+        // the canvas
+        // see http://developer.android.com/reference/android/graphics/PorterDuff.Mode.html
+        // see http://en.wikipedia.org/wiki/Alpha_compositing
+        // see
+        // http://groups.google.com/group/android-developers/browse_thread/thread/5b0a498664b17aa0/de4aab6fb7e97e38?lnk=gst&q=erase+transparent&pli=1
+        circlePaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SCREEN));
+        // circlePaint.setAlpha(0);
+        circlePaint.setColor(Color.RED);
+        circlePaint.setStyle(Style.FILL_AND_STROKE);
+
+        // TODO remove - DEBUG code
+        i = -1000000000;
+        paint1.setARGB(90, 0, 0, 255); // blue
+        paint1.setAntiAlias(true);
+        // TODO remove - DEBUG code
+
     }
 
-    // @Override
-    // public boolean draw(Canvas canvas, MapView mapView, boolean shadow, long when) {
-    // // TODO Auto-generated method stub
-    // return super.draw(canvas, mapView, shadow, when);
-    // }
-
     @Override
-    public boolean draw(Canvas canvas, MapView mapView, boolean shadow, long when ) {
+    public void draw(Canvas canvas, MapView mapView, boolean shadow) {
+        super.draw(canvas, mapView, shadow);
+        canvas.drawText("redraw? " + i++, 5, 10, paint1);
 
+        Log.d(TAG, "########Called");
         assert !shadow : "The overlay does not need a shadow as it only covers the explored areas.";
+        Projection projection = mapView.getProjection();
 
-        if (pointsUpdated) {
-            // translate the GeoPoint to screen pixels
-            screenPts = new ArrayList<Point>(geoPoints.size());
-            for (GeoPoint geoPoint : geoPoints) {
-
-                // BUG - do not use
-                // point = mapView.getProjection().toPixels(geoPoint, null);
-                // returns an incorrect value in point
-                // you'll cry debugger tears if you do
-                Point point = new Point();
-                mapView.getProjection().toPixels(geoPoint, point);
-                Log.v(TAG, "GeoPoint: " + geoPoint + " converts to screen point: " + point);
-                // for processing add only visible points
-                if (point.x >= 0 && point.x <= mapView.getWidth() && point.y >= 0
-                        && point.y <= mapView.getHeight()) {
-                    screenPts.add(point);
-                }
-
-            }
-            Log.d(TAG, "Number of screen points added: " + screenPts.size());
-        }
-        //	
         // the size of the displayed area is dependent on the zoom level
         // 1 - 19 levels
         // level 19 = 4 pixels/meter
@@ -80,42 +95,47 @@ public class ExploredOverlay extends Overlay {
 
         Log.v(TAG, "View distance is " + viewDistance + " meters, radius in pixels is " + radius
                 + " pixel per meter is " + pixelsMeter);
-        //TODO research what PorterDuffMode does and set to simulate transparency
-        Paint rectPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-
-        rectPaint.setColor(Color.DKGRAY);
-        rectPaint.setAlpha(50);
-        rectPaint.setStyle(Style.FILL_AND_STROKE);
+        // TODO research what PorterDuffMode does and set to simulate transparency
         Rect screenCover = new Rect(0, 0, mapView.getMeasuredWidth(), mapView.getMeasuredHeight());
         canvas.drawRect(screenCover, rectPaint);
 
-        Paint circlePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        // set PorterDuff mode in order to create transparent holes in
-        // the canvas
-        // see http://developer.android.com/reference/android/graphics/PorterDuff.Mode.html
-        // see http://en.wikipedia.org/wiki/Alpha_compositing
-        // see
-        // http://groups.google.com/group/android-developers/browse_thread/thread/5b0a498664b17aa0/de4aab6fb7e97e38?lnk=gst&q=erase+transparent&pli=1
-        circlePaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SCREEN));
-//        circlePaint.setAlpha(0);
-        circlePaint.setColor(Color.RED);
-        circlePaint.setStyle(Style.FILL_AND_STROKE);
 
-        for (Point point : screenPts) {
-            canvas.drawCircle(point.x, point.y, radius, circlePaint);
+        if (pointsUpdated) {
+
+            for (AproximateLocation location : locations) {
+                // BUG - do not use
+                // point = mapView.getProjection().toPixels(geoPoint, null);
+                // returns an incorrect value in point
+                // you'll cry debugger tears if you do
+                projection.toPixels(locationToGeoPoint(location), tempPoint);
+                Log.v(TAG, "GeoPoint to screen point: " + tempPoint);
+                // for display use only visible points
+//                if (tempPoint.x >= 0 && tempPoint.x <= mapView.getWidth() && tempPoint.y >= 0
+//                        && tempPoint.y <= mapView.getHeight()) {
+                    canvas.drawCircle(tempPoint.x, tempPoint.y, radius, circlePaint);
+//                }
+
+            }
+            //draw blue location circle
+            projection.toPixels(new GeoPoint((int) (currentLat * 1e6), (int) (currentLong * 1e6)), tempPoint);
+            canvas.drawCircle(tempPoint.x, tempPoint.y,
+                    radius, currentPaint);
+
         }
 
+
         pointsUpdated = false;
-        // super.draw(canvas, mapView, false);
-        return false;
+        // super.draw(canvadb des, mapView, false);
     }
 
     public void setExplored(List<AproximateLocation> locations) {
-        geoPoints.clear();
-        Log.d(TAG, "Number of points to be displayed is :" + locations.size());
-        for (AproximateLocation location : locations) {
-            geoPoints.add(locationToGeoPoint(location));
-        }
+        this.locations = locations;
         pointsUpdated = true;
+
+    }
+
+    public void setCurrent(double currentLat, double currentLong) {
+        this.currentLat = currentLat;
+        this.currentLong = currentLong;
     }
 }
